52c27cbbc3fd9660b35bff2a47ef0ae83fc07477
parada <parada@perfenterprise.com>
2018-08-18 11:09:25 +0700

    Add project files.

---
+++ VoteMeIfYouCan/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VoteMeIfYouCan.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
