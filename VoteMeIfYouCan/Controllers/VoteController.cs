e4c7c1893c9ae82b9e7c9505375370a91441ae6d
Perawatt <pekung4433@perfenterprise.com>
2018-08-18 11:22:05 +0700

    add view

---
+++ VoteMeIfYouCan/Controllers/VoteController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VoteMeIfYouCan.Controllers
+{
+    public class VoteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Topics()
+        {
+            return View();
+        }
+        public IActionResult AddTopic()
+        {
+            return View();
+        }
+        public IActionResult VoteChoice()
+        {
+            return View();
+        }
+        public IActionResult ViewVoted()
+        {
+            return View();
+        }
+        public IActionResult AddChoice()
+        {
+            return View();
+        }
+    }
+}
