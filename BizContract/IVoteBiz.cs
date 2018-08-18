b4f9499b7a59829ac2523ef6c233a13f0dfbe829
parada <parada@perfenterprise.com>
2018-08-18 14:46:23 +0700

    Move code

--- BizContract/IVoteBiz.cs
+++ BizContract/IVoteBiz.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using SharedViewModel;
 
 namespace BizContract
 {
     public interface IVoteBiz
     {
-
+        Task<IEnumerable<TopicViewModel>> ListTopic(string userName);
+        Task<IEnumerable<ChoiceViewModel>> ListChoice(string topicId);
+        Task<IEnumerable<VoteViewModel>> GetVoteDetail(string topicId, string choiceId);
     }
 }
