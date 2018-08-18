95bc45278b45be5f5dd03d402b398fa09f533283
Perawatt <pekung4433@perfenterprise.com>
2018-08-18 14:28:33 +0700

    add ViewModel

---
+++ VoteMeIfYouCan/Models/TopicViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VoteMeIfYouCan.Models
+{
+    public class TopicViewModel
+    {
+    }
+}
