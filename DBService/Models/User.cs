89e4c0918a0e9fb2cdf162fb846a14c64b48180e
parada <parada@perfenterprise.com>
2018-08-18 11:46:10 +0700

    Add service (not complete)
    Signed-off-by: parada <parada@perfenterprise.com>

---
+++ DBService/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DBService.Models
+{
+    public class User
+    {
+        [BsonId]
+        public int _id { get; set; }
+    }
+}
---
+++ DBService/User.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DBService
+{
+    public class User
+    {
+    }
+}
