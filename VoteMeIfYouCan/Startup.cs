e34a29ed80485fc8c5f7e40bd53037d5c7a83b7f
Perawatt <pekung4433@perfenterprise.com>
2018-08-18 14:14:37 +0700

    add img and implement UI

--- VoteMeIfYouCan/Startup.cs
+++ VoteMeIfYouCan/Startup.cs
@@ -56,7 +56,7 @@ namespace VoteMeIfYouCan
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Vote}/{action=Index}/{id?}");
             });
         }
     }
